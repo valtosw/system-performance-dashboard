bc3d70f91071f8f2b0e8917afd1b5b28cdbe006e
valtosw <volodijbessarabov@knu.ua>
2025-10-06 23:55:48 +0300

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,47 @@
+using Server.Hubs;
+using Server.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR();
+
+builder.Services.AddHostedService<PerformanceMonitoringService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHub<PerformanceHub>("/performanceHub");
+
+app.Run();
---
+++ Server/Services/PerformanceMonitoringService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Server.Services
+{
+    public sealed class PerformanceMonitoringService : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
