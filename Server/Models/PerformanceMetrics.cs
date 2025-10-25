5f1466cb94bdca6135f50acecee1e647ba1bbbe5
valtosw <volodijbessarabov@knu.ua>
2025-10-25 12:23:49 +0300

    Add sealed PerformanceMetrics class for CPU and memory usage

---
+++ Server/Models/PerformanceMetrics.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Models
+{
+    public sealed class PerformanceMetrics
+    {
+        public double CpuUsage { get; set; }
+        public double MemoryUsage { get; set; }
+    }
+}
