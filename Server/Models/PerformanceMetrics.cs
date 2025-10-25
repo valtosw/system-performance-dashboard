10f314721d9d123dfa16e32f18076127f135ae83
valtosw <volodijbessarabov@knu.ua>
2025-10-25 12:56:32 +0300

    Enhance PerformanceMetrics with new system properties

--- Server/Models/PerformanceMetrics.cs
+++ Server/Models/PerformanceMetrics.cs
@@ -4,5 +4,8 @@
     {
         public double CpuUsage { get; set; }
         public double MemoryUsage { get; set; }
+        public double AvailableMemoryGb { get; set; }
+        public int TotalProcesses { get; set; }
+        public double SystemUptimeSec { get; set; }
     }
 }
