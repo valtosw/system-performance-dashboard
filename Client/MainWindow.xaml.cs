d6cb87604a1dc5f9c968172699a178f09777ce00
valtosw <volodijbessarabov@knu.ua>
2025-10-25 12:56:50 +0300

    Refactor and enhance performance dashboard UI/logic

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -16,26 +16,24 @@ namespace Client
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private const string ServerUrl = "https://localhost:7079";
-
         private HubConnection _hubConnection;
         private readonly HttpClient _httpClient;
         private readonly DispatcherTimer _pollTimer;
 
         private double _cpuUsage;
-        public double CpuUsage
-        {
-            get => _cpuUsage;
-            set { _cpuUsage = value; OnPropertyChanged(); }
-        }
+        public double CpuUsage { get => _cpuUsage; set { _cpuUsage = value; OnPropertyChanged(); } }
 
         private double _memoryUsage;
-        public double MemoryUsage
-        {
-            get => _memoryUsage;
-            set { _memoryUsage = value; OnPropertyChanged(); }
-        }
+        public double MemoryUsage { get => _memoryUsage; set { _memoryUsage = value; OnPropertyChanged(); } }
 
-        public double TotalMemoryGb { get; } = 8.0;
+        private double _availableMemoryGb;
+        public double AvailableMemoryGb { get => _availableMemoryGb; set { _availableMemoryGb = value; OnPropertyChanged(); } }
+
+        private int _totalProcesses;
+        public int TotalProcesses { get => _totalProcesses; set { _totalProcesses = value; OnPropertyChanged(); } }
+
+        private double _systemUptimeSec;
+        public double SystemUptimeSec { get => _systemUptimeSec; set { _systemUptimeSec = value; OnPropertyChanged(); } }
 
         public MainWindow()
         {
@@ -46,39 +44,25 @@ namespace Client
             _pollTimer.Tick += PollTimer_Tick;
         }
 
-        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            await ConnectWithWebSockets();
-        }
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e) => await ConnectWithWebSockets();
 
         private async void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             if (!IsLoaded) return;
-
-            var selectedMethod = (sender as RadioButton)?.Content.ToString();
+            var selected = (sender as RadioButton)?.Content.ToString();
             await Disconnect();
-
-            switch (selectedMethod)
+            switch (selected)
             {
-                case "Web Sockets":
-                    await ConnectWithWebSockets();
-                    break;
-                case "Long Polling":
-                    await ConnectWithLongPolling();
-                    break;
-                case "Frequent Polls":
-                    StartFrequentPolling();
-                    break;
+                case "Web Sockets": await ConnectWithWebSockets(); break;
+                case "Long Polling": await ConnectWithLongPolling(); break;
+                case "Frequent Polls": StartFrequentPolling(); break;
             }
         }
 
         private async Task ConnectWithWebSockets()
         {
             _hubConnection = new HubConnectionBuilder()
-                .WithUrl($"{ServerUrl}/performanceHub", options =>
-                {
-                    options.Transports = HttpTransportType.WebSockets;
-                })
+                .WithUrl($"{ServerUrl}/performanceHub", o => o.Transports = HttpTransportType.WebSockets)
                 .Build();
             await StartSignalRConnection();
         }
@@ -86,58 +70,51 @@ namespace Client
         private async Task ConnectWithLongPolling()
         {
             _hubConnection = new HubConnectionBuilder()
-                .WithUrl($"{ServerUrl}/performanceHub", options =>
-                {
-                    options.Transports = HttpTransportType.LongPolling;
-                })
+                .WithUrl($"{ServerUrl}/performanceHub", o => o.Transports = HttpTransportType.LongPolling)
                 .Build();
             await StartSignalRConnection();
         }
 
         private async Task StartSignalRConnection()
         {
-            _hubConnection.On<PerformanceMetrics>("ReceivePerformanceData", (metrics) =>
+            _hubConnection.On<PerformanceMetrics>("ReceivePerformanceData", m =>
             {
                 Dispatcher.Invoke(() =>
                 {
-                    CpuUsage = metrics.CpuUsage;
-                    MemoryUsage = metrics.MemoryUsage;
+                    CpuUsage = m.CpuUsage;
+                    MemoryUsage = m.MemoryUsage;
+                    AvailableMemoryGb = m.AvailableMemoryGb;
+                    TotalProcesses = m.TotalProcesses;
+                    SystemUptimeSec = m.SystemUptimeSec;
                 });
             });
 
-            try
-            {
-                await _hubConnection.StartAsync();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"SignalR Connection Error: {ex.Message}");
-            }
+            try { await _hubConnection.StartAsync(); }
+            catch (Exception ex) { MessageBox.Show($"SignalR Error: {ex.Message}"); }
         }
 
-        private void StartFrequentPolling()
-        {
-            _pollTimer.Start();
-        }
+        private void StartFrequentPolling() => _pollTimer.Start();
 
         private async void PollTimer_Tick(object sender, EventArgs e)
         {
             try
             {
-                var metrics = await _httpClient.GetFromJsonAsync<PerformanceMetrics>("/api/performance/metrics");
-                if (metrics != null)
+                var m = await _httpClient.GetFromJsonAsync<PerformanceMetrics>("/api/performance/metrics");
+                if (m != null)
                 {
-                    CpuUsage = metrics.CpuUsage;
-                    MemoryUsage = metrics.MemoryUsage;
+                    CpuUsage = m.CpuUsage;
+                    MemoryUsage = m.MemoryUsage;
+                    AvailableMemoryGb = m.AvailableMemoryGb;
+                    TotalProcesses = m.TotalProcesses;
+                    SystemUptimeSec = m.SystemUptimeSec;
                 }
             }
             catch (Exception ex)
             {
                 _pollTimer.Stop();
-                MessageBox.Show($"Frequent Polling Error: {ex.Message}");
+                MessageBox.Show($"Polling Error: {ex.Message}");
             }
         }
-
 
         private async Task Disconnect()
         {
@@ -147,32 +124,28 @@ namespace Client
                 await _hubConnection.DisposeAsync();
                 _hubConnection = null;
             }
-            if (_pollTimer.IsEnabled)
-            {
-                _pollTimer.Stop();
-            }
+            if (_pollTimer.IsEnabled) _pollTimer.Stop();
 
             CpuUsage = 0;
             MemoryUsage = 0;
+            AvailableMemoryGb = 0;
+            TotalProcesses = 0;
+            SystemUptimeSec = 0;
         }
 
-        private async void MainWindow_Closing(object sender, CancelEventArgs e)
-        {
-            await Disconnect();
-        }
+        private async void MainWindow_Closing(object sender, CancelEventArgs e) => await Disconnect();
 
-        #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
-        {
+        protected void OnPropertyChanged([CallerMemberName] string name = null) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-        #endregion
     }
 
     public class PerformanceMetrics
     {
         public double CpuUsage { get; set; }
         public double MemoryUsage { get; set; }
+        public double AvailableMemoryGb { get; set; }
+        public int TotalProcesses { get; set; }
+        public double SystemUptimeSec { get; set; }
     }
 }
