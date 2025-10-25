43f5fc054cad7a6c3e8584724cfd65104810fe44
valtosw <volodijbessarabov@knu.ua>
2025-10-25 12:24:14 +0300

    Add PerformanceController for metrics API endpoint

---
+++ Server/Controllers/PerformanceController.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+using Server.Services;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public sealed class PerformanceController : ControllerBase
+    {
+        [SupportedOSPlatform("windows")]
+        [HttpGet("metrics")]
+        public ActionResult<PerformanceMetrics> GetMetrics()
+        {
+            return Ok(PerformanceMonitoringService.LatestMetrics);
+        }
+    }
+}
